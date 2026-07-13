2054977de689e7b1056880eabdace4399469a5cb
Matthew Tang <matthewtang@berkeley.edu>
2026-07-13 00:58:22 -0700

    Updates

---
+++ precache-manifest.4fad75120e8e8c1c3a284bbcec11716b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073d85f3da22aa65b7307f12a5d5f335",
+    "url": "/anime-scav-hunt/index.html"
+  },
+  {
+    "revision": "d212b363dec95d712b53",
+    "url": "/anime-scav-hunt/static/css/main.55805575.chunk.css"
+  },
+  {
+    "revision": "192b3978242ff1e8ab6d",
+    "url": "/anime-scav-hunt/static/js/2.6c6507cb.chunk.js"
+  },
+  {
+    "revision": "d212b363dec95d712b53",
+    "url": "/anime-scav-hunt/static/js/main.59442cf4.chunk.js"
+  },
+  {
+    "revision": "a0acc192b2d03a74a26b",
+    "url": "/anime-scav-hunt/static/js/runtime~main.a65c7eac.js"
+  }
+]);
